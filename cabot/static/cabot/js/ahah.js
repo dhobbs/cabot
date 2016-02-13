41e22a00b3d9e5b745034500bab0e34c88563748
Darren Hobbs <darrenhobbs@mac.com>
2016-02-13 20:03:41 +0000

    Can't poll service list due to datatable issue.

--- cabot/static/cabot/js/ahah.js
+++ cabot/static/cabot/js/ahah.js
@@ -2,21 +2,21 @@
 /**
  * Created by darren on 13/02/2016.
  */
-function refreshActiveComponent(sourceUri, containerId, contentId, interval) {
+function refreshActiveComponent(sourceUri, containerId, contentId, interval, cb) {
     interval = interval || 10000;
     $(document).ready(function() {poll(sourceUri, containerId, contentId, interval)});
 
     function poll(sourceUri, containerId, contentId, interval) {
         setTimeout(function() {
-            refreshComponent(sourceUri, containerId, contentId);
+            refreshComponent(sourceUri, containerId, contentId, cb);
             poll(sourceUri, containerId, contentId, interval);
         }, interval);
     }
 
-    function refreshComponent(sourceUri, containerId, contentId) {
+    function refreshComponent(sourceUri, containerId, contentId, cb) {
         //console.log("Refreshing: " + contentId + " from " + sourceUri);
         $("#"+containerId).load(sourceUri + " #" + contentId, function() {
             //console.log("Container refreshed");
-        });
+        }, cb);
     }
 }
